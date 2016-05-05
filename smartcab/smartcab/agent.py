dd4120bc24d0ed91aaa7c59b36a5ac8155fcf10a
xzenggit <xzengemail@gmail.com>
2016-05-05 18:39:49 -0400

    docs: met specification

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -2,6 +2,7 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+import numpy as np
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -11,10 +12,21 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.Q = {}
+        self.learning_rate = 0.9
+        self.discount_factor = 0.3
+        self.past_state = None
+        self.past_reward = 0.0
+        self.past_action = None
+        self.total = 0
+        self.success = 0
+        self.net_reward = 0.0
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+        self.net_reward = 0.0
+
 
     def update(self, t):
         # Gather inputs
@@ -23,14 +35,54 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        
+        state =(self.next_waypoint, inputs['light'], inputs['oncoming'], inputs['left'])
+        self.state = state
+
+        # If (state, action) not in Q, add it.
+        # random.seed(9001)
+        for x in self.env.valid_actions:
+            if (state, x) not in self.Q:
+                self.Q[(state, x)] = 3.0
+
         # TODO: Select action according to your policy
-        action = None
+        Q_action = []
+        for x in self.env.valid_actions:
+            Q_action.append(self.Q[(state, x)])
+        # epsilon-greedy method
+        epsilon = 0.1
+        if random.random() < epsilon:
+            action = random.choice(self.env.valid_actions)  # random move
+        elif Q_action.count(max(Q_action)) > 1:
+            # if more than one max value, pick one randomly
+            tmp = np.array(Q_action)
+            ind = np.where(tmp==max(tmp))
+            action =self.env.valid_actions[random.choice(ind[0])]
+        else:
+            action = self.env.valid_actions[np.argmax(Q_action)]  # pick the best action from current state-based Q-values
 
         # Execute action and get reward
         reward = self.env.act(self, action)
+        self.net_reward += reward
+
+        # Measure how many times your agent reaches destination before deadline passed.
+        add_total = False
+        if deadline == 0:
+            add_total = True
+        if reward > 5:
+            self.success += 1
+            add_total = True
+        if add_total:
+            self.total += 1
+            print("success: {} / {}. Net reward is {}.".format(self.success, self.total, self.net_reward))
+
 
         # TODO: Learn policy based on state, action, reward
+        if self.past_state != None:
+            self.Q[(self.past_state, self.past_action)] = (1 - self.learning_rate) * self.Q[(self.past_state, self.past_action)] + \
+            self.learning_rate * (self.past_reward + self.discount_factor * max(Q_action))
+        self.past_state = state
+        self.past_action = action
+        self.past_reward = reward
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
@@ -41,15 +93,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
-    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
-    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
-
-    sim.run(n_trials=100)  # run for a specified number of trials
-    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
+    sim = Simulator(e, update_delay=0.1)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=100)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
--- smartcab/smartcab/simulator.py
+++ smartcab/smartcab/simulator.py
@@ -1,13 +1,10 @@
 import os
 import time
 import random
-import importlib
+import pygame
 
 class Simulator(object):
-    """Simulates agents in a dynamic smartcab environment.
-
-    Uses PyGame to display GUI, if available.
-    """
+    """PyGame-based simulator to create a dynamic environment."""
 
     colors = {
         'black'   : (  0,   0,   0),
@@ -21,11 +18,12 @@ class Simulator(object):
         'orange'  : (255, 128,   0)
     }
 
-    def __init__(self, env, size=None, update_delay=1.0, display=True):
+    def __init__(self, env, size=None, frame_delay=10, update_delay=1.0):
         self.env = env
         self.size = size if size is not None else ((self.env.grid_size[0] + 1) * self.env.block_size, (self.env.grid_size[1] + 1) * self.env.block_size)
         self.width, self.height = self.size
-        
+        self.frame_delay = frame_delay
+
         self.bg_color = self.colors['white']
         self.road_width = 5
         self.road_color = self.colors['black']
@@ -36,28 +34,17 @@ class Simulator(object):
         self.last_updated = 0.0
         self.update_delay = update_delay
 
-        self.display = display
-        if self.display:
-            try:
-                self.pygame = importlib.import_module('pygame')
-                self.pygame.init()
-                self.screen = self.pygame.display.set_mode(self.size)
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.size)
 
-                self.frame_delay = max(1, int(self.update_delay * 1000))  # delay between GUI frames in ms (min: 1)
-                self.agent_sprite_size = (32, 32)
-                self.agent_circle_radius = 10  # radius of circle, when using simple representation
-                for agent in self.env.agent_states:
-                    agent._sprite = self.pygame.transform.smoothscale(self.pygame.image.load(os.path.join("images", "car-{}.png".format(agent.color))), self.agent_sprite_size)
-                    agent._sprite_size = (agent._sprite.get_width(), agent._sprite.get_height())
+        self.agent_sprite_size = (32, 32)
+        self.agent_circle_radius = 10  # radius of circle, when using simple representation
+        for agent in self.env.agent_states:
+            agent._sprite = pygame.transform.smoothscale(pygame.image.load(os.path.join("images", "car-{}.png".format(agent.color))), self.agent_sprite_size)
+            agent._sprite_size = (agent._sprite.get_width(), agent._sprite.get_height())
 
-                self.font = self.pygame.font.Font(None, 28)
-                self.paused = False
-            except ImportError as e:
-                self.display = False
-                print "Simulator.__init__(): Unable to import pygame; display disabled.\n{}: {}".format(e.__class__.__name__, e)
-            except Exception as e:
-                self.display = False
-                print "Simulator.__init__(): Error initializing GUI objects; display disabled.\n{}: {}".format(e.__class__.__name__, e)
+        self.font = pygame.font.Font(None, 28)
+        self.paused = False
 
     def run(self, n_trials=1):
         self.quit = False
@@ -68,34 +55,30 @@ class Simulator(object):
             self.last_updated = 0.0
             self.start_time = time.time()
             while True:
+                self.current_time = time.time() - self.start_time
+                #print "Simulator.run(): current_time = {:.3f}".format(self.current_time)
                 try:
-                    # Update current time
-                    self.current_time = time.time() - self.start_time
-                    #print "Simulator.run(): current_time = {:.3f}".format(self.current_time)
-
-                    # Handle GUI events
-                    if self.display:
-                        for event in self.pygame.event.get():
-                            if event.type == self.pygame.QUIT:
+                    # Handle events
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            self.quit = True
+                        elif event.type == pygame.KEYDOWN:
+                            if event.key == 27:  # Esc
                                 self.quit = True
-                            elif event.type == self.pygame.KEYDOWN:
-                                if event.key == 27:  # Esc
-                                    self.quit = True
-                                elif event.unicode == u' ':
-                                    self.paused = True
+                            elif event.unicode == u' ':
+                                self.paused = True
 
-                        if self.paused:
-                            self.pause()
+                    if self.paused:
+                        self.pause()
 
                     # Update environment
                     if self.current_time - self.last_updated >= self.update_delay:
                         self.env.step()
                         self.last_updated = self.current_time
 
-                    # Render GUI and sleep
-                    if self.display:
-                        self.render()
-                        self.pygame.time.wait(self.frame_delay)
+                    # Render and sleep
+                    self.render()
+                    pygame.time.wait(self.frame_delay)
                 except KeyboardInterrupt:
                     self.quit = True
                 finally:
@@ -112,16 +95,16 @@ class Simulator(object):
         # Draw elements
         # * Static elements
         for road in self.env.roads:
-            self.pygame.draw.line(self.screen, self.road_color, (road[0][0] * self.env.block_size, road[0][1] * self.env.block_size), (road[1][0] * self.env.block_size, road[1][1] * self.env.block_size), self.road_width)
+            pygame.draw.line(self.screen, self.road_color, (road[0][0] * self.env.block_size, road[0][1] * self.env.block_size), (road[1][0] * self.env.block_size, road[1][1] * self.env.block_size), self.road_width)
 
         for intersection, traffic_light in self.env.intersections.iteritems():
-            self.pygame.draw.circle(self.screen, self.road_color, (intersection[0] * self.env.block_size, intersection[1] * self.env.block_size), 10)
+            pygame.draw.circle(self.screen, self.road_color, (intersection[0] * self.env.block_size, intersection[1] * self.env.block_size), 10)
             if traffic_light.state:  # North-South is open
-                self.pygame.draw.line(self.screen, self.colors['green'],
+                pygame.draw.line(self.screen, self.colors['green'],
                     (intersection[0] * self.env.block_size, intersection[1] * self.env.block_size - 15),
                     (intersection[0] * self.env.block_size, intersection[1] * self.env.block_size + 15), self.road_width)
             else:  # East-West is open
-                self.pygame.draw.line(self.screen, self.colors['green'],
+                pygame.draw.line(self.screen, self.colors['green'],
                     (intersection[0] * self.env.block_size - 15, intersection[1] * self.env.block_size),
                     (intersection[0] * self.env.block_size + 15, intersection[1] * self.env.block_size), self.road_width)
 
@@ -133,19 +116,19 @@ class Simulator(object):
             agent_color = self.colors[agent.color]
             if hasattr(agent, '_sprite') and agent._sprite is not None:
                 # Draw agent sprite (image), properly rotated
-                rotated_sprite = agent._sprite if state['heading'] == (1, 0) else self.pygame.transform.rotate(agent._sprite, 180 if state['heading'][0] == -1 else state['heading'][1] * -90)
+                rotated_sprite = agent._sprite if state['heading'] == (1, 0) else pygame.transform.rotate(agent._sprite, 180 if state['heading'][0] == -1 else state['heading'][1] * -90)
                 self.screen.blit(rotated_sprite,
-                    self.pygame.rect.Rect(agent_pos[0] - agent._sprite_size[0] / 2, agent_pos[1] - agent._sprite_size[1] / 2,
+                    pygame.rect.Rect(agent_pos[0] - agent._sprite_size[0] / 2, agent_pos[1] - agent._sprite_size[1] / 2,
                         agent._sprite_size[0], agent._sprite_size[1]))
             else:
                 # Draw simple agent (circle with a short line segment poking out to indicate heading)
-                self.pygame.draw.circle(self.screen, agent_color, agent_pos, self.agent_circle_radius)
-                self.pygame.draw.line(self.screen, agent_color, agent_pos, state['location'], self.road_width)
+                pygame.draw.circle(self.screen, agent_color, agent_pos, self.agent_circle_radius)
+                pygame.draw.line(self.screen, agent_color, agent_pos, state['location'], self.road_width)
             if agent.get_next_waypoint() is not None:
                 self.screen.blit(self.font.render(agent.get_next_waypoint(), True, agent_color, self.bg_color), (agent_pos[0] + 10, agent_pos[1] + 10))
             if state['destination'] is not None:
-                self.pygame.draw.circle(self.screen, agent_color, (state['destination'][0] * self.env.block_size, state['destination'][1] * self.env.block_size), 6)
-                self.pygame.draw.circle(self.screen, agent_color, (state['destination'][0] * self.env.block_size, state['destination'][1] * self.env.block_size), 15, 2)
+                pygame.draw.circle(self.screen, agent_color, (state['destination'][0] * self.env.block_size, state['destination'][1] * self.env.block_size), 6)
+                pygame.draw.circle(self.screen, agent_color, (state['destination'][0] * self.env.block_size, state['destination'][1] * self.env.block_size), 15, 2)
 
         # * Overlays
         text_y = 10
@@ -154,18 +137,18 @@ class Simulator(object):
             text_y += 20
 
         # Flip buffers
-        self.pygame.display.flip()
+        pygame.display.flip()
 
     def pause(self):
         abs_pause_time = time.time()
         pause_text = "[PAUSED] Press any key to continue..."
         self.screen.blit(self.font.render(pause_text, True, self.colors['cyan'], self.bg_color), (100, self.height - 40))
-        self.pygame.display.flip()
+        pygame.display.flip()
         print pause_text  # [debug]
         while self.paused:
-            for event in self.pygame.event.get():
-                if event.type == self.pygame.KEYDOWN:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
                     self.paused = False
-            self.pygame.time.wait(self.frame_delay)
+            pygame.time.wait(self.frame_delay)
         self.screen.blit(self.font.render(pause_text, True, self.bg_color, self.bg_color), (100, self.height - 40))
         self.start_time += (time.time() - abs_pause_time)
